cc7834bf76e5f9262cc9cec55e8a3628ac217a31
Student Name <student@example.com>
2026-03-29 01:08:26 +0300

    Remove invalid include('courses.urls') from urls.py

--- config/urls.py
+++ config/urls.py
@@ -3,12 +3,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularSwaggerView,
-    SpectacularRedocView
-)
-
 from users.views import (
     PaymentViewSet,
     UserProfileViewSet,
@@ -27,11 +21,6 @@ router.register(r'lessons', LessonViewSet, basename='lessons')
 router.register(r'subscriptions', SubscriptionViewSet, basename='subscriptions')
 
 urlpatterns = [
-
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -46,5 +35,4 @@ urlpatterns = [
     path('api/courses/<int:pk>/unsubscribe/',
          SubscriptionViewSet.as_view({'post': 'unsubscribe'}),
          name='course-unsubscribe'),
-    path('api/', include('courses.urls')),
 ]
